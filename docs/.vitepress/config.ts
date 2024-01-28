2ebad50a614c0c02a7c6be5b0d1a5d278b4bb4b1
jcbhmr <jcbhmr@outlook.com>
2024-01-27 22:24:00 -0600

    save

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vitepress'
+import { pagefindPlugin } from 'vitepress-plugin-pagefind'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Fontist Formulas",
+  description: "Index of all Fontist Formulas",
+
+  vite: {
+    plugins: [pagefindPlugin()]
+  },
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    // https://vitepress.dev/reference/default-theme-search
+    // search: {
+    //   provider: 'local',
+    //   // https://vitepress.dev/reference/default-theme-search#example-excluding-pages-from-search
+    //   options: {
+    //     _render(src, env, md) {
+    //       const html = md.render(src, env)
+    //       if (env.frontmatter?.search === false) return ''
+    //       if (env.relativePath.match(/(^|\/)guide($|\/)/)) return ''
+    //       console.debug(`Indexing ${env.relativePath} for search!`)
+    //       return html
+    //     }
+    //   }
+    // },
+
+    sidebar: {
+      "/guide/": [
+        {
+          text: 'Guide',
+          items: [
+            { text: 'Create a Formula', link: '/guide/create-formula' },
+          ]
+        }
+      ]
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+<script setup>
+import Search from "vitepress-plugin-pagefind/Search.vue"
+</script>
+
+<Search />
+
+<style>
+  /* We need to "infect" the '.VPNavBar' internal styles. To do that we need
+  to NOT be in 'scoped' mode. But since we aren't in 'scoped' mode we need to
+  make sure we don't accidentally override styles for the actual '.VPNavBar'
+  search box (that one we want to stay normal). Thus, we prefix all our stuff
+  so that it only affects search boxes in '.vp-doc' (the main body) which is
+  crucially ONLY THIS PAGE. */
+  .vp-doc .VPNavBarSearch #local-search {
+    flex-grow: 1 !important;
+  }
+  .vp-doc .VPNavBarSearch .DocSearch-Button {
+    border-color: var(--vp-c-brand-1) !important;
+    background: var(--vp-c-bg-alt) !important;
+}
+</style>
